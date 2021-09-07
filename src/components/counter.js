c35a3d1cdef63ffb578fbf737e0756f17e343508
lewleal <lewleal2012@gmail.com>
2021-09-07 15:37:15 -0300

    Counter styles

--- src/components/counter.js
+++ src/components/counter.js
@@ -1,26 +1,28 @@
-import React, { Component } from "react";
-
-const Counter = (props) =>  {
+import React from "react";
+import { MdSchedule } from "react-icons/md";
+import "./counter.css"
+const Counter = (props) => {
     return (
-      <div className="container">
-        <div className="row">
-        <div className="title">
-          <h1>Contador Simple ReactJS</h1>
+        <div className="container">
+            <div className="row">
+                <div className="title">
+                    <h1>Contador Simple ReactJS</h1>
+                </div>
+                <div className="counter">
+                    <div>
+                        <MdSchedule />
+                        {props.horas +
+                            " horas" +
+                            " " +
+                            props.minutos +
+                            " minutos" +
+                            " " +
+                            props.segundos +
+                            " segundos"}
+                    </div>
+                </div>
+            </div>
         </div>
-        <div className="counter">
-          <p>
-            {props.horas +
-              " horas" +
-              " " +
-              props.minutos +
-              " minutos" +
-              " " +
-              props.segundos +
-              " segundos"}
-          </p>
-        </div>
-        </div>
-      </div>
     );
-  }
+}
 export default Counter;
